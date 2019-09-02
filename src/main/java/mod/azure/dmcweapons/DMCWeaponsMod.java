e2bdf469bcc90d7baa92939c929cdd9f35a54f0a
AzureZhen <rcraft@cyberconnects2.com>
2019-09-02 03:13:38 -0400

    v1.0.8

--- src/main/java/mod/azure/dmcweapons/DMCWeaponsMod.java
+++ src/main/java/mod/azure/dmcweapons/DMCWeaponsMod.java
@@ -22,8 +22,8 @@ public class DMCWeaponsMod {
 
 	public static final String modid = "dmcweapons";
 	public static final String MODNAME = "Devil May Cry Weapons";
-	public static final String version = "1.0.7";
-	public static final String dependencies = "required-after:ebwizardry;after:mmorpg";
+	public static final String version = "1.0.8";
+	public static final String dependencies = "required-after:mmorpg";
 	
 	@SidedProxy(clientSide = "mod.azure.dmcweapons.proxy.ClientProxy", serverSide = "mod.azure.dmcweapons.proxy.CommonProxy")
     public static CommonProxy proxy;
