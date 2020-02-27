6f3033fc82bb44315a32e0789c59abd0aa125f4d
AzureZhen <rcraft@cyberconnects2.com>
2020-02-27 05:52:55 -0500

    Almost got the datapack system for Mine and Slash finished.

--- src/main/java/mod/azure/dmcweapons/DMCWeaponsMod.java
+++ src/main/java/mod/azure/dmcweapons/DMCWeaponsMod.java
@@ -6,13 +6,16 @@ import org.apache.logging.log4j.Logger;
 import mod.azure.dmcweapons.config.Config;
 import mod.azure.dmcweapons.util.LootHandler;
 import mod.azure.dmcweapons.util.MMORPGHandler;
+import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
@@ -41,7 +44,18 @@ public class DMCWeaponsMod {
 
 	private void enqueueIMC(final InterModProcessEvent event) {
 		if (ModList.get().isLoaded("mmorpg") && Config.SERVER.USE_COMPATIBILITY_ON_ITEMS.get()) {
-			MinecraftForge.EVENT_BUS.register(new MMORPGHandler());
+			// MinecraftForge.EVENT_BUS.register(new MMORPGHandler());
+		}
+	}
+
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class GatherDataSubscriber {
+		@SubscribeEvent
+		public static void gatherData(GatherDataEvent event) {
+			DataGenerator gen = event.getGenerator();
+			if (event.includeServer()) {
+				gen.addProvider(new MMORPGHandler().getDataPackCreator(gen));
+			}
 		}
 	}
 }
