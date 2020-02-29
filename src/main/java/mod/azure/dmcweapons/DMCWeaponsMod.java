059e0b5c989c997d43a00fa6c4202fd2062fb64b
AzureZhen <rcraft@cyberconnects2.com>
2020-02-28 19:36:17 -0500

    v1.0.0

--- src/main/java/mod/azure/dmcweapons/DMCWeaponsMod.java
+++ src/main/java/mod/azure/dmcweapons/DMCWeaponsMod.java
@@ -6,16 +6,14 @@ import org.apache.logging.log4j.Logger;
 import mod.azure.dmcweapons.config.Config;
 import mod.azure.dmcweapons.util.LootHandler;
 import mod.azure.dmcweapons.util.MMORPGHandler;
-import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
 
@@ -30,6 +28,7 @@ public class DMCWeaponsMod {
 		ModLoadingContext modLoadingContext = ModLoadingContext.get();
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::setup);
+		modEventBus.addListener(this::enqueueIMC);
 		modLoadingContext.registerConfig(ModConfig.Type.SERVER, Config.SERVER_SPEC, "dmcweapons-config.toml");
 		Config.loadConfig(Config.SERVER_SPEC, FMLPaths.CONFIGDIR.get().resolve("dmcweapons-config.toml").toString());
 	}
@@ -40,16 +39,9 @@ public class DMCWeaponsMod {
 		}
 	}
 
-	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-	public static class GatherDataSubscriber {
-		@SubscribeEvent
-		public static void gatherData(GatherDataEvent event) {
-			DataGenerator gen = event.getGenerator();
-			if (event.includeServer()) {
-				if (ModList.get().isLoaded("mmorpg") && Config.SERVER.USE_COMPATIBILITY_ON_ITEMS.get()) {
-					gen.addProvider(new MMORPGHandler().getDataPackCreator(gen));
-				}
-			}
+	private void enqueueIMC(final InterModProcessEvent event) {
+		if (ModList.get().isLoaded("mmorpg") && Config.SERVER.USE_COMPATIBILITY_ON_ITEMS.get()) {
+			MinecraftForge.EVENT_BUS.register(new MMORPGHandler());
 		}
 	}
 }
--- src/main/java/mod/azure/dmcweapons/util/MMORPGHandler.java
+++ src/main/java/mod/azure/dmcweapons/util/MMORPGHandler.java
@@ -1,77 +1,30 @@
 package mod.azure.dmcweapons.util;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import com.robertx22.mine_and_slash.config.compatible_items.OldConfigItem;
-import com.robertx22.mine_and_slash.data_generation.compatible_items.CompatibleItem;
-import com.robertx22.mine_and_slash.data_generation.compatible_items.CompatibleItemProvider;
+import com.robertx22.mine_and_slash.api.MineAndSlashAPI;
+import com.robertx22.mine_and_slash.config.compatible_items.ConfigItem;
 import com.robertx22.mine_and_slash.database.gearitemslots.weapons.Bow;
 import com.robertx22.mine_and_slash.database.gearitemslots.weapons.Sword;
-import com.robertx22.mine_and_slash.onevent.data_gen.providers.SlashDataProvider;
 
 import mod.azure.dmcweapons.config.Config;
-import net.minecraft.data.DataGenerator;
 
 public class MMORPGHandler {
 
-	public static String ID = "compatible_items";
-
 	public MMORPGHandler() {
+		MineAndSlashAPI.addCompatibleItem("dmcweapons:rebellion", new ConfigItem().setType(Sword.INSTANCE)
+				.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
+		MineAndSlashAPI.addCompatibleItem("dmcweapons:redqueen", new ConfigItem().setType(Sword.INSTANCE)
+				.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
+		MineAndSlashAPI.addCompatibleItem("dmcweapons:spada", new ConfigItem().setType(Sword.INSTANCE)
+				.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
+		MineAndSlashAPI.addCompatibleItem("dmcweapons:yamato", new ConfigItem().setType(Sword.INSTANCE)
+				.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
+		MineAndSlashAPI.addCompatibleItem("dmcweapons:devilsworddante", new ConfigItem().setType(Sword.INSTANCE)
+				.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
+		MineAndSlashAPI.addCompatibleItem("dmcweapons:coyote-a", new ConfigItem().setType(Bow.INSTANCE)
+				.setSalvagable(true).setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
+		MineAndSlashAPI.addCompatibleItem("dmcweapons:ivory", new ConfigItem().setType(Bow.INSTANCE).setSalvagable(true)
+				.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
+		MineAndSlashAPI.addCompatibleItem("dmcweapons:ebony", new ConfigItem().setType(Bow.INSTANCE).setSalvagable(true)
+				.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
 	}
-
-	@SuppressWarnings("rawtypes")
-	public SlashDataProvider getDataPackCreator(DataGenerator gen) {
-		return new CompatibleItemProvider(gen, getList(), ID);
-	}
-
-	public static List<CompatibleItem> getList() {
-		List<String> SWORDS = Stream.of("dmcweapons:rebellion", "dmcweapons:redqueen", "dmcweapons:spada",
-				"dmcweapons:devilsworddante", "dmcweapons:yamato").collect(Collectors.toList());
-
-		List<String> GUNS = Stream.of("dmcweapons:coyotea", "dmcweapons:ivory", "dmcweapons:ebony",
-				"dmcweapons:kalinaann1", "dmcweapons:kalinaann2").collect(Collectors.toList());
-
-		List<CompatibleItem> items = new ArrayList<>();
-		try {
-			for (int type = 1; type <= 1; type++)
-				switch (type) {
-				case 1:
-					for (String sword : SWORDS) {
-						OldConfigItem config = new OldConfigItem().setGenerationWeights(1000, 200, 0).setMaxRarity(5)
-								.setMinRarity(0).setSalvagable(true).setType(Sword.INSTANCE)
-								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
-
-						String id = sword;
-
-						CompatibleItem neww = config.convertToNewFormat();
-						neww.guid = id;
-						neww.item_id = id;
-
-						items.add(neww);
-					}
-				case 2:
-					for (String gun : GUNS) {
-						OldConfigItem bowconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0).setMaxRarity(5)
-								.setMinRarity(0).setSalvagable(true).setType(Bow.INSTANCE)
-								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
-
-						String id2 = gun;
-
-						CompatibleItem bow = bowconfig.convertToNewFormat();
-						bow.guid = id2;
-						bow.item_id = id2;
-
-						items.add(bow);
-					}
-				}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return items;
-	}
-
 }
